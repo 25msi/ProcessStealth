a682852371d10f41cff118b2129e5d2891b7aa78
25msi <94462616+25msi@users.noreply.github.com>
2021-11-16 15:14:06 +0100

    Update main.c

--- src/64bit/main.c
+++ src/64bit/main.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
     wchar_t Target[] = L"Taskmgr.exe";
-    wchar_t Hide[] = L"chrome.exe";
+    wchar_t Hide[] = L"csgo.exe";
     ProcessStealth(Target, Hide);
     system("pause");
 }
